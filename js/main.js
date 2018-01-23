3b70b6afc6611101897fa8fdeb36a67848af1483
NguyenThaiAnh <thaianhkhtn@gmail.com>
2018-01-23 08:49:41 +0700

    - section our team

--- js/main.js
+++ js/main.js
@@ -21,4 +21,63 @@ $(document).ready(function () {
             anchorBuilder: false
         }
     });
+
+
+    $('.portfolio-carousel').carouFredSel({
+
+        responsive:true,
+        width:'100%',
+        circular:true,
+        prev:'#prev',
+        next:'#next',
+        scroll:{
+            items:1,
+            duration:500,
+            pauseOnHover: true
+        },
+        auto:true,
+        items:{
+            visible:{
+                min:1,
+                max:4
+            },
+            height:"variable"
+        }
+
+    });
+
+
+    $('.team-carousel').carouFredSel({
+
+        responsive:true,
+        width:'100%',
+        circular:true,
+        prev:'#team-prev',
+        next:'#team-next',
+        scroll:{
+            items:1,
+            duration:500,
+            pauseOnHover: true
+        },
+        auto:true,
+        items:{
+            visible:{
+                min:1,
+                max:4
+            },
+            height:"variable"
+        }
+
+    });
+
+
+
+    $(window).scroll(function () {
+        var top = $(window).scrollTop();
+        if (top >= 112){
+            $("header").addClass("secondary-dark-blue-bg")
+        } else if ($('header').hasClass("secondary-dark-blue-bg")){
+            $('header').removeClass("secondary-dark-blue-bg")
+        }
+    })
 });
